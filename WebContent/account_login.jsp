8c606d5db4cee83adef4ab7f966c94a99129a08c
LuHuaiwei <804490378@qq.com>
2018-04-30 10:35:02 +0800

    v1.0

--- WebContent/account_login.jsp
+++ WebContent/account_login.jsp
@@ -6,13 +6,31 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html">
-<title>欢迎光临</title>
+	<title>欢迎光临</title>
+	<style>
+		div{
+			text-align:center;
+		}
+	</style>
 </head>
 <body>
-	<%ResultSet rs = (ResultSet)request.getAttribute("result");%>
-	ID is:<%rs.getString(1); %><br>
-	name is:<%rs.getString(2); %><br>
-	password is:<%rs.getString(3); %><br>
+	<div>
 	<br>
+	登录成功！
+	<br><br><br><br><br>
+	<%String s1 = null; %>
+	<%String s2 = null; %>
+	<%String s3 = null; %>
+	<%ResultSet rs = (ResultSet)request.getAttribute("result");%>
+	<%if(rs.next()){
+		s1 = rs.getString(1);	
+		s2 = rs.getString(2);
+		s3 = rs.getString(3);
+	} %>
+	身份证： is:<%=s1%><br>
+	名字:<%=s2%><br>
+	密码： is:<%=s3%><br>
+	<br>
+	</div>
 </body>
 </html>
--- src/theway/Theway.java
+++ src/theway/Theway.java
@@ -25,27 +25,32 @@ public class Theway {
 		MySQLBase mbs = new MySQLBase();
 		Connection con1 = mbs.linking();
 		System.out.println("即将输入用户名");
-		String sql = "select password from user where username = '?'";
+		String sql = "select password from user where username =?";
 		PreparedStatement ps = con1.prepareCall(sql);
 		ps.setString(1,username);
-		System.out.println("获取密码集合");
 		ResultSet rs = ps.executeQuery();
-		System.out.println("开始判断获取密码");
-		System.out.println(rs.getRow());
-		if(password == rs.getString(1)) {
-			rs.close();
-			ps.close();
-			con1.close();
-			System.out.println("获取密码成功");
-			return true;
+		System.out.println("获取密码集合");
+		while(rs.next()) {
+			System.out.println("开始判断获取密码");
+			System.out.println("数据库中获取的密码："+rs.getString(1));
+			if(rs.getString(1).equals(password)) {
+				rs.close();
+				ps.close();
+				con1.close();
+				System.out.println("获取密码成功");
+				return true;
+			}
+			else{
+				System.out.println("用户输入的密码："+password);
+				rs.close();
+				ps.close();
+				con1.close();
+				System.out.println("获取密码失败");
+				return false;
+			}
 		}
-		else{
-			rs.close();
-			ps.close();
-			con1.close();
-			System.out.println("获取密码失败");
-			return false;
-		}
+		return true;
+
 	}
 	public static ResultSet getall(String name) throws Exception {
 		MySQLBase mbs = new MySQLBase();
